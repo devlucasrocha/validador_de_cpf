8176c64b2f71976b63ae3837dbcfacdfe1019b1c
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-04-01 18:04:11 -0300

    Replacing functions to arrow functions

--- js/analyzer.js
+++ js/analyzer.js
@@ -1,8 +1,6 @@
 const cpf = document.getElementById('cpf')
 
-function getLenOf(cpf) {
-    return cpf.value.length
-}
+const getLenOf = cpf => cpf.value.length
 
 cpf.addEventListener('input', () => {
     if (getLenOf(cpf) === 14) {
@@ -20,56 +18,56 @@ cpf.addEventListener('input', () => {
         $("#main-box").removeClass("border-success")
         $("#main-box").removeClass("border-danger")
         $("#main-box").addClass("border-secondary")
-
     }
 })
 
-function validate_cpf(cpf) {
+const validate_cpf = cpf => {
     let cpf_array = []
     cpf_array = cpf.split('')
     const getNumbers = e => e != '.' && e != '-'
     cpf_array = cpf_array.filter(getNumbers).map((e) => parseInt(e))
 
-    let primeiro_digito = getPrimeiroDigito(cpf_array) * 10 % 11
-    let segundo_digito = getSegundoDigito(cpf_array) * 10 % 11
-
-    primeiro_digito = primeiro_digito === 10 ? 0 : primeiro_digito
-    segundo_digito = segundo_digito === 10 ? 0 : segundo_digito
+    let primeiro_digito = getPrimeiroDigito(cpf_array)
+    let segundo_digito = getSegundoDigito(cpf_array)
     console.log(`d1: ${primeiro_digito} d2: ${segundo_digito}`)
 
-    function todosIguais(count = 0) {
-        for (let i = 9; i >= 0; i--) {
-            count = cpf_array.filter(x => x == i).length
-            if (count == 11) {
-                break
-            }
-        }
-        if (count == 11) {
-            return true
-        }
-    }
-
-    function getSegundoDigito(cpf_array, dec11 = 11, calc = 0) {
-        for (let c = 0; c < cpf_array.length - 1; c++) {
-            calc += cpf_array[c] * dec11
-            dec11--
-        }
-        return calc
-    }
-
-    function getPrimeiroDigito(cpf_array, dec10 = 10, calc = 0) {
-        for (let c = 0; c < cpf_array.length - 2; c++) {
-            calc += cpf_array[c] * dec10
-            dec10--
-        }
-        return calc
-    }
-
-    const validacao = primeiro_digito === cpf_array[9] && segundo_digito === cpf_array[10] && !todosIguais()
+    const validacao = primeiro_digito === cpf_array[9] && segundo_digito === cpf_array[10] && !todosIguais(cpf_array)
     return validacao
 }
 
-function setStatus(status_msg) {
+const getPrimeiroDigito = (cpf_array, dec10 = 10, calc = 0) => {
+    for (let c = 0; c < cpf_array.length - 2; c++) {
+        calc += cpf_array[c] * dec10
+        dec10--
+    }
+    calc = calc * 10 % 11
+    calc = calc === 10 ? 0 : calc
+    return calc
+}
+
+const getSegundoDigito = (cpf_array, dec11 = 11, calc = 0) => {
+    for (let c = 0; c < cpf_array.length - 1; c++) {
+        calc += cpf_array[c] * dec11
+        dec11--
+    }
+    calc = calc * 10 % 11
+    calc = calc === 10 ? 0 : calc
+    return calc
+}
+
+const todosIguais = (cpf_array, count = 0) => {
+    for (let i = 9; i >= 0; i--) {
+        count = cpf_array.filter(x => x == i).length
+        if (count == 11) {
+            break
+        }
+    }
+    if (count == 11) {
+        return true
+    }
+}
+
+const setStatus = (status_msg) => {
     $('#status').html(status_msg)
     if (status_msg.includes(' VÁLIDO')) {
         $('#status').removeClass('text-danger')
